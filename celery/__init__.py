8e2ccdef81fa2d35dddd8c15a18418824e713ff8
Tei Im <tei.im@sendbird.com>
2020-08-11 16:55:28 +0900

    Fix __version__

--- celery/__init__.py
+++ celery/__init__.py
@@ -24,7 +24,7 @@ from . import local  # noqa
 
 SERIES = 'cliffs'
 
-__version__ = '4.4.6'
+__version__ = '4.4.6-gevent-enforced'
 __author__ = 'Ask Solem'
 __contact__ = 'auvipy@gmail.com'
 __homepage__ = 'http://celeryproject.org'
